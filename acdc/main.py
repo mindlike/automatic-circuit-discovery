dafc1c1a6737f0ac04e94ee2d9b2653ad9821826
Bertram Hojer <bertramhojer@gmail.com>
2024-03-17 10:48:02 +0100

    new 'reasoning' task added to repo

---
+++ acdc/main.py
@@ -0,0 +1,151 @@
+import torch
+import gc
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from transformer_lens.hook_points import HookedRootModule, HookPoint
+from transformer_lens.HookedTransformer import (
+    HookedTransformer,
+)
+
+from acdc.docstring.utils import get_all_docstring_things
+from acdc.ioi.utils import get_all_ioi_things
+from acdc.reasoning.utils import get_all_reasoning_things
+
+from acdc.acdc_utils import (
+    make_nd_dict,
+    reset_network,
+    shuffle_tensor,
+    cleanup,
+    ct,
+    TorchIndex,
+    Edge,
+    EdgeType,
+)  # these introduce several important classes !!!
+
+from acdc.TLACDCExperiment import TLACDCExperiment
+
+from acdc.acdc_utils import kl_divergence
+from acdc.induction.utils import get_all_induction_things
+import argparse
+
+torch.autograd.set_grad_enabled(False)
+
+### Parser arguments for command-line
+"""
+--task=induction --zero-ablation --threshold=0.71 --indices-mode=reverse --first-cache-cpu=False --second-cache-cpu=False --max-num-epochs=100 --device=cpu
+"""
+
+TASK = "reasoning"
+
+THRESHOLD = 0.71  # only used if >= 0.0
+ZERO_ABLATION = True
+INDICES_MODE = "reverse"
+NAMES_MODE = "normal"
+DEVICE = "cpu"
+METRIC = "kl_div"
+RESET_NETWORK = 0
+SINGLE_STEP = False
+
+second_metric = None  # some tasks only have one metric
+
+if TASK == "reasoning":
+    num_examples = 5
+    things = get_all_reasoning_things(
+        num_examples=num_examples, device=DEVICE, metric_name=METRIC
+    )
+elif TASK == "ioi":
+    num_examples = 5
+    things = get_all_ioi_things(
+        num_examples=num_examples, device=DEVICE, metric_name=METRIC
+    )
+elif TASK == "induction":
+    num_examples = 4
+    seq_len = 300
+    things = get_all_induction_things(
+        num_examples=num_examples, seq_len=seq_len, device=DEVICE, metric=METRIC
+    )
+
+validation_metric = things.validation_metric # metric we use (e.g KL divergence)
+toks_int_values = things.validation_data # clean data x_i
+toks_int_values_other = things.validation_patch_data # corrupted data x_i'
+tl_model = things.tl_model # transformerlens model
+
+if RESET_NETWORK:
+    reset_network(TASK, DEVICE, tl_model)
+
+tl_model.reset_hooks()
+
+# Save some mem
+gc.collect()
+torch.cuda.empty_cache()
+
+tl_model.reset_hooks()
+
+exp = TLACDCExperiment(
+    model=tl_model,
+    threshold=THRESHOLD,
+    zero_ablation=ZERO_ABLATION,
+    abs_value_threshold=THRESHOLD,
+    ds=toks_int_values,
+    ref_ds=toks_int_values_other,
+    metric=validation_metric,
+    second_metric=second_metric,
+    verbose=True,
+    indices_mode=INDICES_MODE,
+    names_mode=NAMES_MODE,
+    hook_verbose=False,
+    add_sender_hooks=True,
+    #use_pos_embed=use_pos_embed,
+    add_receiver_hooks=False,
+    remove_redundant=False,
+    #show_full_index=use_pos_embed,
+)
+
+
+for i in range(num_examples):
+    exp.step(testing=False)
+
+    print(i, "-" * 50)
+    print(exp.count_no_edges())
+
+    if i == 0:
+        exp.save_edges("edges.pkl")
+
+exp.save_edges("another_final_edges.pkl")
+
+exp.save_subgraph(
+    return_it=True,
+)
+
+
+"""
+parser = argparse.ArgumentParser(description="Used to launch ACDC runs. Only task and threshold are required")
+
+task_choices = ['ioi', 'docstring', 'induction', 'tracr-reverse', 'tracr-proportion', 'greaterthan', 'or_gate']
+parser.add_argument('--task', type=str, required=True, choices=task_choices, help=f'Choose a task from the available options: {task_choices}')
+parser.add_argument('--threshold', type=float, required=True, help='Value for THRESHOLD')
+parser.add_argument('--first-cache-cpu', type=str, required=False, default="True", help='Value for FIRST_CACHE_CPU (the old name for the `online_cache`)')
+parser.add_argument('--second-cache-cpu', type=str, required=False, default="True", help='Value for SECOND_CACHE_CPU (the old name for the `corrupted_cache`)')
+parser.add_argument('--zero-ablation', action='store_true', help='Use zero ablation')
+parser.add_argument('--using-wandb', action='store_true', help='Use wandb')
+parser.add_argument('--wandb-entity-name', type=str, required=False, default="remix_school-of-rock", help='Value for WANDB_ENTITY_NAME')
+parser.add_argument('--wandb-group-name', type=str, required=False, default="default", help='Value for WANDB_GROUP_NAME')
+parser.add_argument('--wandb-project-name', type=str, required=False, default="acdc", help='Value for WANDB_PROJECT_NAME')
+parser.add_argument('--wandb-run-name', type=str, required=False, default=None, help='Value for WANDB_RUN_NAME')
+parser.add_argument("--wandb-dir", type=str, default="/tmp/wandb")
+parser.add_argument("--wandb-mode", type=str, default="online")
+parser.add_argument('--indices-mode', type=str, default="normal")
+parser.add_argument('--names-mode', type=str, default="normal")
+parser.add_argument('--device', type=str, default="cuda")
+parser.add_argument('--reset-network', type=int, default=0, help="Whether to reset the network we're operating on before running interp on it")
+parser.add_argument('--metric', type=str, default="kl_div", help="Which metric to use for the experiment")
+parser.add_argument('--torch-num-threads', type=int, default=0, help="How many threads to use for torch (0=all)")
+parser.add_argument('--seed', type=int, default=1234)
+parser.add_argument("--max-num-epochs",type=int, default=100_000)
+parser.add_argument('--single-step', action='store_true', help='Use single step, mostly for testing')
+parser.add_argument("--abs-value-threshold", action='store_true', help='Use the absolute value of the result to check threshold')
+"""
---
+++ acdc/reasoning/utils.py
@@ -0,0 +1,222 @@
+from collections import OrderedDict
+from dataclasses import dataclass
+import warnings
+from functools import partial
+from copy import deepcopy
+import torch.nn.functional as F
+from typing import List
+import click
+import IPython
+from acdc.acdc_utils import MatchNLLMetric, frac_correct_metric, logit_diff_metric, kl_divergence, negative_log_probs
+import torch
+from acdc.docstring.utils import AllDataThings
+from tqdm import tqdm
+from transformer_lens.HookedTransformer import HookedTransformer
+from dataclasses import dataclass
+from typing import (
+    List,
+    Tuple,
+    Dict,
+    Any,
+    Optional,
+    Callable
+)
+
+def get_gpt2_small(device="cuda") -> HookedTransformer:
+    """
+    Load a pre_trained HookedTransformer model from the transformer_lens package.
+    """
+    tl_model = HookedTransformer.from_pretrained("gpt2")
+    tl_model = tl_model.to(device)
+    tl_model.set_use_attn_result(True)
+    tl_model.set_use_split_qkv_input(True)
+    if "use_hook_mlp_in" in tl_model.cfg.to_dict():
+        tl_model.set_use_hook_mlp_in(True)
+    return tl_model
+
+def get_ioi_gpt2_small(device="cuda"):
+    """For backwards compatibility"""
+    return get_gpt2_small(device=device)
+
+def load_prompts(filepath: str):
+    """
+    Read in all texts in a .txt file. Each line is a new prompt that should be stored in a tuple with (prompt, correct_answer, wrong_answer).
+    """
+    with open(filepath, 'r') as file:
+        prompts = [tuple(line.strip().split('\t')) for line in file]
+    
+    return prompts
+
+
+def get_all_reasoning_things(num_examples, device, metric_name, kl_return_one_element=True):
+
+    # Load the pre-trained model
+    tl_model = get_gpt2_small(device=device)
+
+    # Load the prompts
+    clean_data = [d for d in load_prompts("data/yesno_train.txt") if len(d[0]) < 105]
+    assert len(clean_data) >= num_examples * 2
+
+    # limit #examples to to twice num_examples to have validation and test set
+    clean_data = clean_data[:num_examples*2]
+
+    # Load the corrupt data ### NEEDS TO BE A DIFFERENT FILE (?)
+    corrupt_data = [d for d in load_prompts("data/yesno_train.txt") if len(d[0]) < 105]
+    assert len(corrupt_data) >= num_examples * 2
+    corrupt_data = corrupt_data[:num_examples*2]
+
+    tl_model.tokenizer.padding_side = "left"
+
+    # extract only the prompt (no answer)
+    clean_data = [d[0] for d in clean_data]
+    # tokenize questions with left padding -- the padding means we needn't worry about seq_len
+    clean_input = tl_model.tokenizer(clean_data, pad_to_max_length=True, return_tensors="pt")
+    # tokenize answers
+    clean_answers = tl_model.tokenizer([" " + d[1] for d in clean_data], return_tensors="pt")
+
+    corrupt_data = [d[0] for d in corrupt_data]
+    corrupt_input = tl_model.tokenizer(corrupt_data, pad_to_max_length=True, return_tensors="pt")
+    corrupt_answers = tl_model.tokenizer([" " + d[1] for d in corrupt_data], return_tensors="pt")
+
+    # tokenized objects contain inputs_ids and an attention mask
+    default_data = clean_input["input_ids"].to(device)
+    patch_data = corrupt_input["input_ids"].to(device)
+
+    labels = clean_answers["input_ids"].to(device)
+    corrupt_labels = corrupt_answers["input_ids"].to(device)
+
+    validation_data = default_data[:num_examples]
+    validation_patch_data = patch_data[:num_examples]
+    validation_labels = labels[:num_examples]
+    validation_corrupt_labels = corrupt_labels[:num_examples]
+
+    test_data = default_data[num_examples:]
+    test_patch_data = patch_data[num_examples:]
+    test_labels = labels[num_examples:]
+    test_wrong_labels = corrupt_labels[num_examples:]
+
+    with torch.no_grad():
+        #base_model_logits = tl_model(default_data)[:, -1, :]
+        base_model_logits = tl_model(default_data)[:, -1, :]
+        base_model_logprobs = F.log_softmax(base_model_logits, dim=-1)
+
+    base_validation_logprobs = base_model_logprobs[:num_examples, :]
+    base_test_logprobs = base_model_logprobs[num_examples:, :]
+
+
+    if metric_name == "kl_div":
+        validation_metric = partial(
+            kl_divergence,
+            base_model_logprobs=base_validation_logprobs,
+            last_seq_element_only=True,
+            base_model_probs_last_seq_element_only=False,
+            return_one_element=kl_return_one_element,
+        )
+    elif metric_name == "logit_diff":
+        validation_metric = partial(
+            logit_diff_metric,
+            correct_labels=validation_labels,
+            wrong_labels=validation_corrupt_labels,
+        )
+    elif metric_name == "frac_correct":
+        validation_metric = partial(
+            frac_correct_metric,
+            correct_labels=validation_labels,
+            wrong_labels=validation_corrupt_labels,
+        )
+    elif metric_name == "nll":
+        validation_metric = partial(
+            negative_log_probs,
+            labels=validation_labels,
+            last_seq_element_only=True,
+        )
+    elif metric_name == "match_nll":
+        validation_metric = MatchNLLMetric(
+            labels=validation_labels,
+            base_model_logprobs=base_validation_logprobs,
+            last_seq_element_only=True,
+        )
+    else:
+        raise ValueError(f"metric_name {metric_name} not recognized")
+
+    test_metrics = {
+        "kl_div": partial(
+            kl_divergence,
+            base_model_logprobs=base_test_logprobs,
+            last_seq_element_only=True,
+            base_model_probs_last_seq_element_only=False,
+        ),
+        "logit_diff": partial(
+            logit_diff_metric,
+            correct_labels=test_labels,
+            wrong_labels=test_wrong_labels,
+        ),
+        "frac_correct": partial(
+            frac_correct_metric,
+            correct_labels=test_labels,
+            wrong_labels=test_wrong_labels,
+        ),
+        "nll": partial(
+            negative_log_probs,
+            labels=test_labels,
+            last_seq_element_only=True,
+        ),
+        "match_nll": MatchNLLMetric(
+            labels=test_labels,
+            base_model_logprobs=base_test_logprobs,
+            last_seq_element_only=True,
+        ),
+    }
+
+    return AllDataThings(
+        tl_model=tl_model,
+        validation_metric=validation_metric,
+        validation_data=validation_data,
+        validation_labels=validation_labels,
+        validation_mask=None,
+        validation_patch_data=validation_patch_data,
+        test_metrics=test_metrics,
+        test_data=test_data,
+        test_labels=test_labels,
+        test_mask=None,
+        test_patch_data=test_patch_data,
+    )
+
+
+@dataclass(frozen=False)
+class AllDataThings:
+    tl_model: HookedTransformer
+    validation_metric: Callable[[torch.Tensor], torch.Tensor]
+    validation_data: torch.Tensor
+    validation_labels: Optional[torch.Tensor]
+    validation_mask: Optional[torch.Tensor]
+    validation_patch_data: torch.Tensor
+    test_metrics: dict[str, Any]
+    test_data: torch.Tensor
+    test_labels: Optional[torch.Tensor]
+    test_mask: Optional[torch.Tensor]
+    test_patch_data: torch.Tensor
+
+
+@dataclass(frozen=True)
+class Conn:
+    inp: str
+    out: str
+    qkv: tuple[str, ...]
+
+
+
+"""
+    ioi_dataset = IOIDataset(
+        prompt_type="ABBA",
+        N=num_examples*2,
+        nb_templates=1,
+        seed = 0,
+    )
+
+    abc_dataset = (
+        ioi_dataset.gen_flipped_prompts(("IO", "RAND"), seed=1)
+        .gen_flipped_prompts(("S", "RAND"), seed=2)
+        .gen_flipped_prompts(("S1", "RAND"), seed=3)
+    )
+"""
